4db2349d51ab90364f4800df946466d1b2c3662d
João Paulo Nobre Rodrigues <joaopaulonr.ti@gmail.com>
2022-11-08 00:11:34 -0300

    Atividade 02 adicionada.

---
+++ Atividades/Atividade02/Historico.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+sudo mkdir informatica_e_organizacao_de_computadores
+cd informatica_e_organizacao_de_computadores
+sudo ln -s /atividades/atividade02/disciplinas/informatica_e_organizacao_de_computadores.txt programa
+sudo ln -s /atividades/atividade02/professores/marcos_dantas_ortiz.txt professor
+cd ..
+sudo mkdir fundamentos_de_programacao
+cd fundamentos_de_programacao
+sudo ln -s /atividades/atividade02/disciplinas/fundamentos_de_programacao.txt programa
+sudo ln -s /atividades/atividade02/professores/fabio_carlos_sousa_dias.txt professor
+cd ..
+sudo mkdir teoria_geral_da_administracao
+cd teoria_geral_da_administracao
+sudo ln -s /atividades/atividade02/disciplinas/teoria_geral_da_administracao.txt programa
+sudo ln -s /atividades/atividade02/professores/joao_ferreira_de_lavor.txt professor
+cd ..
+sudo mkdir matematica_computacional
+cd matematica_computacional
+sudo ln -s /atividades/atividade02/disciplinas/matematica_computacional.txt programa
+sudo ln -s /atividades/atividade02/professores/francisco_erivelton_fernandes_de_aragao.txt professor
+cd ..
+sudo mkdir metodos_e_tecnicas_de_pesquisa
+cd metodos_e_tecnicas_de_pesquisa
+sudo ln -s /atividades/atividade02/disciplinas/metodos_e_tecnicas_de_pesquisa.txt programa
+sudo ln -s /atividades/atividade02/professores/joao_ferreira_de_lavor.txt professor
+cd ..
+sudo mkdir redes_de_computadores
+cd redes_de_computadores
+sudo ln -s /atividades/atividade02/disciplinas/redes_de_computadores.txt programa
+sudo ln -s /atividades/atividade02/professores/antonio_rafael_braga.txt professor
+cd ..
+sudo mkdir sistemas_operacionais
+cd sistemas_operacionais
+sudo ln -s /atividades/atividade02/disciplinas/sistemas_operacionais.txt programa
+sudo ln -s /atividades/atividade02/professores/thiago_werlley_bandeira_da_silva.txt professor
+cd ..
+sudo mkdir empreendedorismo
+cd empreendedorismo
+sudo ln -s /atividades/atividade02/disciplinas/empreendedorismo.txt programa
+sudo ln -s /atividades/atividade02/professores/joao_ferreira_de_lavor.txt professor
+cd ..
+sudo mkdir probabilidade_e_estatistica
+cd probabilidade_e_estatistica
+sudo ln -s /atividades/atividade02/disciplinas/probabilidade_e_estatistica.txt programa
+sudo ln -s /atividades/atividade02/professores/paulo_armando_cavalcante_aguilar.txt professor
+cd ..
+sudo mkdir internet_e_arquitetura_tcp_ip
+cd internet_e_arquitetura_tcp_ip
+sudo ln -s /atividades/atividade02/disciplinas/internet_e_arquitetura_tcp_ip.txt programa
+sudo ln -s /atividades/atividade02/professores/alisson_barbosa_de_souza.txt professor
+cd ..
+sudo mkdir administracao_de_sistemas_operacionais_windows
+cd administracao_de_sistemas_operacionais_windows
+sudo ln -s /atividades/atividade02/disciplinas/administracao_de_sistemas_operacionais_windows.txt programa
+sudo ln -s /atividades/atividade02/professores/antonio_rafael_braga.txt professor
+cd ..
+sudo mkdir administracao_de_sistemas_operacionais_linux
+cd administracao_de_sistemas_operacionais_linux
+sudo ln -s /atividades/atividade02/disciplinas/administracao_de_sistemas_operacionais_linux.txt programa
+sudo ln -s /atividades/atividade02/professores/jeandro_de_mesquita_bezerra.txt professor
+cd ..
+sudo mkdir fundamentos_de_banco_de_dados
+cd fundamentos_de_banco_de_dados
+sudo ln -s /atividades/atividade02/disciplinas/fundamentos_de_banco_de_dados.txt programa
+sudo ln -s /atividades/atividade02/professores/livia_almada_cruz.txt professor
+cd ..
+sudo mkdir etica_direito_e_legislacao
+cd etica_direito_e_legislacao
+sudo ln -s /atividades/atividade02/disciplinas/etica_direito_e_legislacao.txt programa
+sudo ln -s /atividades/atividade02/professores/wladimir_araujo_tavares.txt professor
