2ee994ba7775b9ee72847eb8dfa3b0d392bb7724
João Paulo Nobre Rodrigues <joaopaulonr.ti@gmail.com>
2022-12-02 21:50:08 -0300

    Atividade 13 atualizada.

--- Atividades/Atividade13/instanciaServico.sh
+++ Atividades/Atividade13/instanciaServico.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
-#Parâmetro para a inserção do nome da key de acesso a instance da AWS.
-KEY=$1
+#Parâmetro-para-a-inserção-da-chave-de-acesso.
+KEY=$1 
+echo "Criando o servidor..."
+#Variáveis-com-comandos-utilizados-no-script.
+ISO="ami-052efd3df9dad4825"
+VPCID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId" --output text)
+#Comando-para-escolher-a-subrede-padrão.
+SUBNET=$(aws ec2 describe-subnets --query "Subnets[].SubnetId[]" --output text | tr "\t" "," | tr "," "\n" | sed '2,6d')
+#Criação-do-grupo-de-segurança.
+aws ec2 create-security-group --group-name Script-group --description "Meu grupo de seguranca" --vpc-id ${VPCID} > /dev/null
+#Pegando-o-ID-do-grudo-de-segurança.
+SECGRUPID=$(aws ec2 describe-security-groups --group-names  "Script-group" --query "SecurityGroups[].GroupId[]" --output text)
+#Criação-das-regras-do-grupo.
+aws ec2 authorize-security-group-ingress --group-id ${SECGRUPID} --protocol tcp --port 22 --cidr 0.0.0.0/0 > /dev/null
+aws ec2 authorize-security-group-ingress --group-id ${SECGRUPID} --protocol tcp --port 80 --cidr 0.0.0.0/0 > /dev/null
+#Criação-da-instância.
+aws ec2 run-instances --image-id ${ISO} --instance-type t2.micro --key-name ${KEY} --subnet-id ${SUBNET} --security-group-ids ${SECGRUPID} --user-data file://service.sh >> aux.txt
+#Pegando-o-ID-da-instância-em-tempo-de-execução.
+InstanceId=$(grep "InstanceId" aux.txt | tr -d '"' | tr -d ','  | cut -d ":" -f2)
+#Pegando-IP-público-a-partir-da-variável-InstanceId.
+IPPUB=$(aws ec2 describe-instances --query "Reservations[].Instances[].PublicIpAddress" --instance-id ${InstanceId} --output text)
+#Contador-de-segundos-para-a-configuração-do-servidor-WEB.
+for ((i = 69; i >= 1; i--)) do
+clear
+echo "Espere alguns segundos para a inicialização do servidor." 
+echo "Finalizando em ${i}s."
+sleep 0.5
+clear
+echo "Espere alguns segundos para a inicialização do servidor.." 
+echo "Finalizando em ${i}s.."
+sleep 0.5
+clear
+echo "Espere alguns segundos para a inicialização do servidor..." 
+echo "Finalizando em ${i}s..."
+done
+clear
+rm -f aux.txt
+#Echo-do-link-de-acesso.
+echo "Acesse: http://${IPPUB}/"
---
+++ Atividades/Atividade13/service.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+sudo apt update > /dev/null
+sudo timedatectl set-timezone America/Fortaleza
+sudo apt install -y apache2 > /dev/null
+cat <<EOF > dados.sh
+#!/bin/bash
+while true
+do
+echo "Informacoes coletadas em: [\$(date '+%d/%m/%Y %T')]." >> index.html
+echo "<br>" >> index.html
+echo "Tempo ativo: \$(uptime -p | cut -d " " -f2) minutos." >> index.html
+echo "<br>" >> index.html
+echo "Memoria livre: \$(free -m | sed 1d | sed 2d | tr -s '[:space:]' ',' | cut -d ',' -f4) MB." >> index.html
+echo "<br>" >> index.html
+echo "Memoria Usada: \$(free -m | sed 1d | sed 2d | tr -s '[:space:]' ',' | cut -d ',' -f3) MB." >> index.html
+echo "<br>" >> index.html
+echo "Bytes recebidos: \$(cat /proc/net/dev | sed 1,3d | tr -s '[:space:]' ',' | cut -d ',' -f3)." >> index.html
+echo "<br>" >> index.html
+echo "Bytes transmitidos: \$(cat /proc/net/dev | sed 1,3d | tr -s '[:space:]' ',' | cut -d ',' -f11)." >> index.html
+mv index.html /var/www/html/
+sleep 5
+done
+EOF
+
+mv dados.sh /home/ubuntu/
+chmod +x /home/ubuntu/dados.sh
+
+cat<<EOF2 > dados.service
+[Unit]
+Description=WebData - Serviço Web de informações da instância.
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+ExecStart=/home/ubuntu/dados.sh
+[Install]
+WantedBy=multi-user.target
+EOF2
+
+mv dados.service /etc/systemd/system
+systemctl enable dados.service
+systemctl start dados.service
